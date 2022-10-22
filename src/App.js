a01cd26e9dbb25034650dca0c34ef9fc2405fdec
FireFly3 <4irelly@gmail.com>
2022-10-22 12:44:41 +0200

    project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import {Header} from "./components";
+// import {Header} from "./components";
+// import {MovieList} from "./components";
+
+
+import {Routes, Route} from "react-router-dom";
+import {MainLayout} from "./layouts";
 import {MovieList} from "./components";
 
-
-
 function App() {
-  return (
-    <div >
-      <Header/>
-        <div>
-            <MovieList/>
-        </div>
-
-    </div>
-  );
+    return (
+        // <div >
+        //   <Header/>
+        //     <div>
+        //         <MovieList/>
+        //     </div>
+        //
+        // </div>
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route path={'getMovies'} element={<MovieList/>}/>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
