d2d4e1c43fa51ed44c685084c067e3372c7c937b
FireFly3 <4irelly@gmail.com>
2022-10-27 18:39:47 +0200

    project

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -3,7 +3,7 @@ import {axiosService} from "./axios.service";
 import {urls} from "../configs";
 
 const movieService = {
-    getAll: (page = 1) => axiosService.get(urls.getMovies, {params: {page}}),
+    getAll: (page = 1) => axiosService.get(urls.getMovies, {params: {page}} ),
     getById: (id)=> axiosService.get(`${urls.getById}/${id}`),
     search:(query)=> axiosService.get(`${urls.searchMovie}`, {params: {query: query}})
 }
